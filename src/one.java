51ae9096c0f7602b71bd432d96d3f1f495e134b3
Romaha <119759507+Romaha1@users.noreply.github.com>
2023-06-04 23:11:10 +0300

    Update one.java

--- src/one.java
+++ src/one.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class one {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String text = " ";
-        for(int i = 0; i < 5; i++){
+        String sentence = "";
+
+        System.out.println("Введіть п'ять слів:");
+
+        for (int i = 0; i < 5; i++) {
             String word = sc.nextLine();
-            text = text.concat(word + " ");
+            sentence = sentence.concat(word + " ");
         }
-        System.out.println(text);
+
+        System.out.println("Результат: " + sentence.trim());
     }
 }
