8bc7bf505e9af80a9dbcf2aa100d29a0b2e52859
Romaha <119759507+Romaha1@users.noreply.github.com>
2023-06-04 23:09:17 +0300

    Update four.java

--- src/four.java
+++ src/four.java
@@ -3,9 +3,15 @@ import java.util.Scanner;
 public class four {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String first = sc.nextLine(), second = sc.nextLine();
+        System.out.println("Введіть перше ім'я:");
+        String first = sc.nextLine().trim();
+        System.out.println("Введіть друге ім'я:");
+        String second = sc.nextLine().trim();
         if(first.equals(second)){
             System.out.println("Імена ідентичні");
-        }else System.out.println("Імена НЕ ідентичні");
+        } else {
+            System.out.println("Імена НЕ ідентичні");
+        }
+        sc.close();
     }
 }
