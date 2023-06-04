db2e9214369bfcd85da223ee4f54bd37ce1caff5
Romaha <119759507+Romaha1@users.noreply.github.com>
2023-06-04 23:12:37 +0300

    Update two.java

--- src/two.java
+++ src/two.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 
 public class two {
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        for(int i = 0; i < 5; i++){
-            String word = sc.nextLine();
-            String letter = word.substring(0,1);
-            System.out.println(letter);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть п'ять слів:");
+
+        for (int i = 0; i < 5; i++) {
+            String word = scanner.next();
+            String firstLetter = word.substring(0, 1);
+            System.out.println("Перша літера слова \"" + word + "\": " + firstLetter);
         }
+
+        scanner.close();
     }
 }
+
